0b14a03a7d327f0aae605e5c08e34f030a41af50
Đỗ Anh Huy <93468444+huyda1610@users.noreply.github.com>
2024-11-26 15:17:41 +0700

    feat: adding frammer motion

---
+++ src/app/(auth)/template.tsx
@@ -0,0 +1,23 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type PropsType = {
+  children: React.ReactNode;
+};
+
+function AuthTemplate({ children }: PropsType) {
+  return (
+    <motion.div
+      initial={{ x: -15, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ ease: 'easeOut', duration: 0.5 }}
+      className="w-full h-full bg-white-background relative"
+      // className="p-4 bg-blue-500 text-white"
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default AuthTemplate;
